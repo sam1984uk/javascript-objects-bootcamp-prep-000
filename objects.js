e7ce71310469d7b67e04ee85380d2249492aa978
sam ditson <samditson2@hotmail.co.uk>
2018-08-02 23:44:11 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { artist:"track" };
+
+function updatePlaylist(playlist, artist, track){
+  playlist[artist] = track
+  return playlist
+} 
+
+function removeFromPlaylist(playlist, artist){
+  return delete playlist.artist;
+}
